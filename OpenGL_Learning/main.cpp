25f5b268b7cdcae66e27123670da605600e90561
MilkP <milkp86@gmail.com>
2018-06-19 15:30:22 +0800

    move, rotation, zoom

--- OpenGL_Learning/main.cpp
+++ OpenGL_Learning/main.cpp
@@ -1,13 +1,20 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
+
 #include "Shader.h"
 #include "Texture.h"
+#include "Camera.h"
 
+//==================================DEFINE=====================================
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 
 const unsigned int WIN_WIDTH = 800;
@@ -26,8 +33,17 @@ const char* fragmentShaderPath = "Shader/shader.fs";
 
 float angle;
 
+Camera mainCamera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = WIN_WIDTH / 2;
+float lastY = WIN_HEIGHT / 2;
+bool firstMouse = true;
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+//================================Main==========================================
 int main() {
-	//Base Setting========================================================
+	//Base Setting & Window Setting========================================================
 	//GLFW Init
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -42,7 +58,10 @@ int main() {
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
 	//GLAD Init
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -50,8 +69,8 @@ int main() {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-
-
+	//END==========================================================================
+	
 	//Setup Shader
 	Shader ourShader(vertexShaderPath, fragmentShaderPath);
 	ourShader.use();
@@ -165,6 +184,11 @@ int main() {
 	//Game Loop
 	//============================================================================
 	while (!glfwWindowShouldClose(window)) {
+		//frame-time set
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		//get input
 		processInput(window);
 
@@ -179,8 +203,16 @@ int main() {
 		//draw
 		ourShader.use();
 
-		//final draw
+		//active VAO
 		glBindVertexArray(VAO);
+
+		projection = glm::perspective(glm::radians(mainCamera.Zoom), (float)WIN_WIDTH / (float)WIN_HEIGHT, 0.1f, 100.0f);
+		ourShader.setMat4("projection", projection);
+
+		// camera/view transformation
+		view = mainCamera.GetViewMatrix();
+		ourShader.setMat4("view", view);
+
 		for (unsigned int i = 0; i < 10; i++)
 		{
 			angle = 20.0f * (i + 1);
@@ -210,8 +242,41 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 
+void mouse_callback(GLFWwindow * window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; 
+
+	lastX = xpos;
+	lastY = ypos;
+
+	mainCamera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow * window, double xoffset, double yoffset)
+{
+	mainCamera.ProcessMouseScroll(yoffset);
+}
+
+
 //Input function
 void processInput(GLFWwindow *window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		mainCamera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		mainCamera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		mainCamera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		mainCamera.ProcessKeyboard(RIGHT, deltaTime);
 }
